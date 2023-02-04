546c0328c93b5fc905b2e0c8c7384f9064e12876
Davi Luna <Sdavi738@gmail.com>
2023-02-04 17:37:16 -0300

    alterando a conexao com o db e a tabela

--- back-end/adicionar-usuario.php
+++ back-end/adicionar-usuario.php
@@ -4,11 +4,11 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $dsn = 'mysql:dbname=web1;host=127.0.0.1;port=3307';
+    $dsn = 'mysql:dbname=web1_avaliacao;host=127.0.0.1;port=3307';
     $conn = new PDO($dsn, 'root', 'root');
 
     // $conn->query("INSERT INTO users(username, password) VALUES ('$username', '$password')");
-    $stmt = $conn->prepare('INSERT INTO users(username, password) VALUES (?, ?)');
+    $stmt = $conn->prepare('INSERT INTO usuario(username, password) VALUES (?, ?)');
     $stmt->execute([$username, $password]);
 
     http_response_code(201);
