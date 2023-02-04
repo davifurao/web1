953a22c37eba0e1a07d19a3aaa55ae384bebe84d
Davi Luna <Sdavi738@gmail.com>
2023-02-04 16:40:32 -0300

    adicionando o stores e o index.

---
+++ front-end/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Index from "./lib/Index.svelte";
+  import { currentPage } from "./stores";
+</script>
+
+<main>
+  <h1>Hello cruel world</h1>
+
+  {#if $currentPage == 'index'}
+    <Index />
+  {/if}
+
+</main>
